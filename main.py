1ed7c1672e30ebf686206bf09b717f04746b5521
cama-ka <karlacamarena91@yahoo.fr>
2021-06-24 14:49:58 +0200

    generer paires pour le tour 1

--- main.py
+++ main.py
@@ -4,35 +4,84 @@ from classRound import Round
 from classMatch import Match
 
 
-dico_joueurs = {"cle":3}
 
-
-Turn1 = Tournaments("Des familles", "Lille", "23/05/2021", "2 min", 2, "description")
-joueur1 = Player("cama","karla", "21/12/1899", "f", 1001)
-joueur2 = Player("cama","Paco", "21/12/1899", "f", 1000)
-joueur3 = Player("cama","Brian", "21/12/1899", "f", 500)
-joueur4 = Player("cama","Bernadette", "21/12/1899", "f", 1500)
-joueur5 = Player("cama","Martine", "21/12/1899", "f", 1001)
-joueur6 = Player("cama","Melina", "21/12/1899", "f", 800)
-joueur7 = Player("cama","Yvette", "21/12/1899", "f", 500)
-joueur8 = Player("cama","Oscar", "21/12/1899", "f", 1400)
-
-liste_joueurs = [joueur1, joueur2, joueur3, joueur4, joueur5, joueur6, joueur7, joueur8]
-
-
-def ajouter_tous_les_joueurs_au_dict(liste):
-    for i in liste:
-        d = {i.get_prenom() : i.get_classement()}
-        dico_joueurs.update(d)
-
-ajouter_tous_les_joueurs_au_dict(liste_joueurs)
-
-print(dico_joueurs)
-
-def creer_pairs():
-        if joueur1.get_classement() == joueur2.get_classement():
-            Round.get_choose_player_1(joueur1)
-        else:
-            print("c'est paaas possssiiiiblleee")
-        
+def infos_tournoi():
+    turn = Tournaments(input("Nom du tournois : "),input("Lieu du tournois : "),\
+            input("Date du tournois : "),input("Nombre de tour du tournois : "),\
+            input("Timing du tournois : "),input("Description du tournois : "))
+     
+    return turn.info_turn()
     
+def infos_joueur():
+    joueur = Player(input("Nom du joueur : "), input("Prénom : "),\
+                input("Date : "), input("Sexe : "), input("Classement : "))
+     
+    return joueur.info_liste()
+    
+    
+def ajout_joueur(liste,func):
+    """ Permet à l'utilisateur d'entrer de nouveaux joueurs """ 
+    
+    while True:
+        try :
+            reponse = input(f"Souhaitez-vous ajouter un nouveau joueur ? ").capitalize()
+            if reponse == "Yes" or reponse == "Oui":
+                ajout_joueur = func()
+                liste.append(ajout_joueur)
+                continue
+            else:
+                print(f"Vous ne souhaitez pas ajouter de nouveau joueur.")
+                break
+                
+        except ValueError:
+            print(f"Le format de votre réponse n'est pas bon. Veuillez recommencer.")
+            break
+            
+           
+
+
+
+def joueur_numerotation(liste, func):
+    """ Sauvegarder des joueurs dans la liste """
+    liste.append(func)
+    
+    
+def trie(liste):
+    """ Trie la liste  """
+    liste.sort(key = lambda x: x[1])
+    return liste
+
+
+def creer_deux_groupes(groupe1, groupe2, liste):
+    """ 1. create 2 group  """
+    
+    for i in liste[:4]:
+        groupe1.append(i)
+    for i in liste[-4:]:
+        groupe2.append(i)
+        
+        
+def creer_4_pairs(groupe1, groupe2):
+    """ 2. create pairs  """
+    pair1 = (groupe1[0], groupe2[0])
+    pair2 = (groupe1[1], groupe2[1])
+    pair3 = (groupe1[2], groupe2[2])
+    pair4 = (groupe1[3], groupe2[3])
+    
+    pairs_tour1 = [pair1, pair2, pair3, pair4]
+    
+    return pairs_tour1
+
+
+
+def main():           
+    liste_ajout_joueur = []
+    groupe1 = []
+    groupe2 = []
+    ajout_joueur(liste_ajout_joueur, infos_joueur)
+    creer_deux_groupes(groupe1, groupe2, liste_ajout_joueur)
+    tour1 = creer_4_pairs(groupe1, groupe2)
+    print(tour1)
+            
+if __name__ == "__main__":
+    main()
